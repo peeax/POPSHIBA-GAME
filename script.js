e5f01aceaa058738757a842438a5294b5412197b
pongsatach boonkaew <pongsatach9@gmail.com>
2023-10-04 12:12:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+    var img = document.getElementById("popdog1");
+    var count = document.getElementById("score");
+    var score = 0;
+    var audio = new Audio('pop.mp3');
+
+    // mouseclick 
+    img.addEventListener('mousedown', function () {
+        increaseScore();
+        img.src = 'popdog2-removebg.png';
+        audio.play();
+    });
+
+    img.addEventListener('mouseup', function () {
+        img.src = 'popdog1-removebg.png';
+        audio.play();
+    });
+
+    function increaseScore() {
+        score++;
+        count.innerHTML = score;
+    }
+}
